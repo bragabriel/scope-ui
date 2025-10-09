f96d96aa2ff34a5b54bfc0274a7e99feedbbc02f
gabriel-braga <gabriel.bragasilva@hotmail.com>
2025-10-09 11:43:08 -0300

    feat: footer, header and homepage adjustments

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { AcoesComponent } from './components/acoes/acoes';
+import { HomeComponent } from './components/home/home';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/acoes', pathMatch: 'full' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
   { path: 'acoes', component: AcoesComponent },
 ];
